21a6cb961b65b4ac215a5dd566c361e50985ab25
Geoff Goetz <geoff.goetz503@gmail.com>
2020-01-17 14:21:01 -0800

    Add CSS styling. Add html code. Add and change JavaScript.

--- js/scripts.js
+++ js/scripts.js
@@ -6,18 +6,28 @@ $(document).ready(function(){
     var cpuChoice = $("#processor").val();
     var gpuChoice = $("#graphics").val();
     var coolingChoice = $("#cooling").val();
-    console.log(osChoice, colorChoice, cpuChoice, gpuChoice, coolingChoice)
+
     if (osChoice === "1" || osChoice === "2" && cpuChoice === "1", "3" && gpuChoice === "1", "2" ) {
       $("#cSharp").show();
       $("#or").show();
       $("#javaScript").show();
       $("#python").hide();
+      $("#cplusplus").hide();
     } else if (osChoice === "4" || coolingChoice === "1") {
       $("#javaScript").hide();
       $("#cSharp").hide();
       $("#python").show();
+      $("#cplusplus").hide();
     } else if (osChoice === "3" || cpuChoice === "2" || gpuChoice === "3") {
-  
+      $("#javaScript").hide();
+      $("#cSharp").hide();
+      $("#python").show();
+      $("#cplusplus").hide();
+    } else if (coolingChoice === "2" || osChoice !== "2", "3") {
+      $("#javaScript").hide();
+      $("#cSharp").hide();
+      $("#python").hide();
+      $("#cplusplus").show();
     } else if (colorChoice === "1", "2", "3", "4" ) {
       $("#allTheAbove").show();
   } else {
