46056f989b15ccf5939fdccd263c2971b52aded2
Geoff Goetz <geoff.goetz503@gmail.com>
2020-01-17 11:45:31 -0800

    Add JS if/else content. Change JS variable.

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,20 @@
 $(document).ready(function(){
   $("form#languageChoices").submit(function(event) {
     event.preventDefault();
-    var languageChoice = parseInt($("input#languages").val());
-    var colorChoice = parseInt($("input#favColors").val());
-    var cpuChoice = parseInt($("input#processor").val());
-    var gpuChoice = parseInt($("input#graphics").val());
-    var coolingChoice = parseInt($("input#cooling").val());
-
-    if (languageChoice === "aa") {
+    var osChoice = $("#os").val();
+    var colorChoice = $("#favColors").val();
+    var cpuChoice = $("#processor").val();
+    var gpuChoice = $("#graphics").val();
+    var coolingChoice = $("#cooling").val();
+    console.log(languageChoice, colorChoice, cpuChoice, gpuChoice, coolingChoice)
+    if (osChoice === "1" ) {
       $("#cSharp").show();
       $("#javaScript").hide();
+      $("#python").hide();
     } else {
     $("#javaScript").show();
     $("#cSharp").hide();
+    $("#python").hide();
   }
   });
 });
