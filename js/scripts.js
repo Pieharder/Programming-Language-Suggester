92768637ce90f8fa5451eff055097feb23193e47
Geoff Goetz <geoff.goetz503@gmail.com>
2020-01-17 11:14:05 -0800

    Add additional JS.

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,18 @@
 $(document).ready(function(){
-  $("#languageChoices").submit(function(event)
+  $("form#languageChoices").submit(function(event) {
+    event.preventDefault();
+    var languageChoice = parseInt($("input#languages").val());
+    var colorChoice = parseInt($("input#favColors").val());
+    var cpuChoice = parseInt($("input#processor").val());
+    var gpuChoice = parseInt($("input#graphics").val());
+    var coolingChoice = parseInt($("input#cooling").val());
+
+    if (languageChoice === "aa") {
+      $("#cSharp").show();
+      $("#javaScript").hide();
+    } else {
+    $("#javaScript").show();
+    $("#cSharp").hide();
+  }
+  });
+});
