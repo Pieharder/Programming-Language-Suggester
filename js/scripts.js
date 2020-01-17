c1e429638c0508379816c645e3be9a156ee70ba0
Geoff Goetz <geoff.goetz503@gmail.com>
2020-01-17 13:49:59 -0800

    Add and change else/if statements. Add else if statement result options.

--- js/scripts.js
+++ js/scripts.js
@@ -6,15 +6,22 @@ $(document).ready(function(){
     var cpuChoice = $("#processor").val();
     var gpuChoice = $("#graphics").val();
     var coolingChoice = $("#cooling").val();
-    console.log(languageChoice, colorChoice, cpuChoice, gpuChoice, coolingChoice)
-    if (osChoice === "1" ) {
+    console.log(osChoice, colorChoice, cpuChoice, gpuChoice, coolingChoice)
+    if (osChoice === "1" || osChoice === "2" && cpuChoice === "1", "3" && gpuChoice === "1", "2" ) {
       $("#cSharp").show();
-      $("#javaScript").hide();
+      $("#or").show();
+      $("#javaScript").show();
       $("#python").hide();
-    } else if (osChoice === "4") {
-    $("#javaScript").hide();
-    $("#cSharp").hide();
-    $("#python").show();
+    } else if (osChoice === "4" || coolingChoice === "1") {
+      $("#javaScript").hide();
+      $("#cSharp").hide();
+      $("#python").show();
+    } else if (osChoice === "3" || cpuChoice === "2" || gpuChoice === "3") {
+  
+    } else if (colorChoice === "1", "2", "3", "4" ) {
+      $("#allTheAbove").show();
+  } else {
+    $("#allTheAbove").show();
   }
   });
 });
