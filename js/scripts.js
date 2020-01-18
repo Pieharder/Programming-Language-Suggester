c411b2edc45aa7b77b23af96cefae0b7748a79d4
Geoff Goetz <geoff.goetz503@gmail.com>
2020-01-17 17:45:27 -0800

    Change from drop down form to radio buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,52 +1,59 @@
 $(document).ready(function() {
   $("form#languageChoices").submit(function(event) {
     event.preventDefault();
-    
-    if ((cpuChoice === "No Preference") || (cpuChoice === "Intel")) {
-      $("#cSharp").show();
-      $("#javaScript").show();
-      $("#python").hide();
-      $("#cPlusPlus").hide();
-    } else (cpuChoice === "AMD") 
-      $("#javaScript").hide();
-      $("#cSharp").hide();
-      $("#python").show();
-      $("#cPlusPlus").hide();
-  });  
-  $("form#languageChoices").submit(function(event) {
-  var osChoice = $("#os").val();
-  if (osChoice === "Apple")
+
+  var cpuChoice = $("#processor").val(); 
+  if ((cpuChoice === "No Preference") || (cpuChoice === "Intel")) {
+    $("#cSharp").show();
+    $("#javaScript").show();
+    $("#python").hide();
+  } else (cpuChoice === "AMD") 
     $("#javaScript").hide();
     $("#cSharp").hide();
     $("#python").show();
-    $("#cPlusPlus").hide();
-  } else ((osChoice === "Windows") || (osChoice === "Linux") || (osChoice === "No Preference"))
+    
+  var osChoice = $("#os").val();
+  if (osChoice === "Linux") {
     $("#javaScript").hide();
-    $("#or").hide();
+    $("#cSharp").hide();
+    $("#python").show();
+  } else ((osChoice === "Windows") || (osChoice === "Apple") || (osChoice === "No Preference")) 
+    $("#javaScript").hide();
+    $("#cSharp").show();
+    $("#python").hide();
+  
+  var colorChoice = $("#favColors").val(); 
+  if (colorChoice === "No Preference") {
+    $("#javaScript").hide();
+    $("#cSharp").hide();
+    $("#python").show();
+  } else ((colorChoice === "Red") || (colorChoice === "Blue") || (colorChoice === "Green")) 
+    $("#javaScript").show();
     $("#cSharp").hide();
     $("#python").hide();
-    $("#cPlusPlus").show();
-});
-
-  $("form#languageChoices").submit(function(event) {
-    var colorChoice = $("#favColors").val(); 
-    if (colorChoice === "No Preference") {
-      $("#javaScript").hide();
-      $("#cSharp").hide();
-      $("#python").hide();
-      $("#cPlusPlus").show();
-      $("#allTheAbove").hide();
-    } else (colorChoice === "Red" || "Blue" || "Green")
-      $("#javaScript").hide();
-      $("#cSharp").hide();
-      $("#python").hide();
-      $("#cPlusPlus").show();
-      $("#allTheAbove").hide();
-  });
 
 
+  var gpuChoice = $("#graphics").val(); 
+  if ((gpuChoice === "No Preference") || (gpuChoice === "Nvidia")) {
+    $("#cSharp").show();
+    $("#javaScript").hide();
+    $("#python").hide();
+  } else (gpuChoice === "Radeon") 
+    $("#javaScript").hide();
+    $("#cSharp").hide();
+    $("#python").show();
 
 
+  var coolingChoice = $("#cooling").val(); 
+  if ((coolingChoice === "No Preference") || (coolingChoice === "Air Cooling")) {
+    $("#cSharp").show();
+    $("#javaScript").hide();
+    $("#python").hide();
+  } else (gpuChoice === "Water Cooling") 
+    $("#javaScript").hide();
+    $("#cSharp").hide();
+    $("#python").show();
+  
   $("#formReset").click(function() {
     $("#javaScript").hide();
     $("#or").hide();
@@ -54,4 +61,6 @@ $(document).ready(function() {
     $("#python").hide();
     $("#cPlusPlus").hide();
     $("#allTheAbove").hide();
+    });   
   });
+});
