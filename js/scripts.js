d42be9e67bce56ea6b38e14dee776a12e16200c8
Geoff Goetz <geoff.goetz503@gmail.com>
2020-01-17 18:50:13 -0800

    Finalized JS. Finalized CSS. Finalized HTML.

--- js/scripts.js
+++ js/scripts.js
@@ -1,66 +1,35 @@
-$(document).ready(function() {
-  $("form#languageChoices").submit(function(event) {
+// User Interface (or front-end) logic:
+$(document).ready(function () {
+  $("form#languageChoices").submit(function (event) {
     event.preventDefault();
 
-  var cpuChoice = $("#processor").val(); 
-  if ((cpuChoice === "No Preference") || (cpuChoice === "Intel")) {
-    $("#cSharp").show();
-    $("#javaScript").show();
-    $("#python").hide();
-  } else (cpuChoice === "AMD") 
-    $("#javaScript").hide();
-    $("#cSharp").hide();
-    $("#python").show();
-    
-  var osChoice = $("#os").val();
-  if (osChoice === "Linux") {
-    $("#javaScript").hide();
-    $("#cSharp").hide();
-    $("#python").show();
-  } else ((osChoice === "Windows") || (osChoice === "Apple") || (osChoice === "No Preference")) 
-    $("#javaScript").hide();
-    $("#cSharp").show();
-    $("#python").hide();
-  
-  var colorChoice = $("#favColors").val(); 
-  if (colorChoice === "No Preference") {
-    $("#javaScript").hide();
-    $("#cSharp").hide();
-    $("#python").show();
-  } else ((colorChoice === "Red") || (colorChoice === "Blue") || (colorChoice === "Green")) 
-    $("#javaScript").show();
-    $("#cSharp").hide();
-    $("#python").hide();
+    var osChoice = $("#os").val();
+    var colorChoice = $("#favColors").val();
+    var cpuChoice = $("#processor").val();
+    var gpuChoice = $("#graphics").val();
+    var coolingChoice = $("#cooling").val();
 
-
-  var gpuChoice = $("#graphics").val(); 
-  if ((gpuChoice === "No Preference") || (gpuChoice === "Nvidia")) {
-    $("#cSharp").show();
-    $("#javaScript").hide();
-    $("#python").hide();
-  } else (gpuChoice === "Radeon") 
-    $("#javaScript").hide();
-    $("#cSharp").hide();
-    $("#python").show();
-
-
-  var coolingChoice = $("#cooling").val(); 
-  if ((coolingChoice === "No Preference") || (coolingChoice === "Air Cooling")) {
-    $("#cSharp").show();
-    $("#javaScript").hide();
-    $("#python").hide();
-  } else (gpuChoice === "Water Cooling") 
-    $("#javaScript").hide();
-    $("#cSharp").hide();
-    $("#python").show();
-  
-  $("#formReset").click(function() {
+    // Business (or back-end) logic:
+    if (osChoice === "Windows") {
+      $("#cSharp").hide();
+      $("#javaScript").show();
+      $("#python").hide();
+    } else if ((cpuChoice === "AMD") || (gpuChoice === Nvidia)) {
+      $("#javaScript").hide();
+      $("#cSharp").hide();
+      $("#python").show();
+    } else {
+      $("#javaScript").hide();
+      $("#cSharp").show();
+      $("#python").hide();
+    }
+  });
+  $("#formReset").click(function () {
     $("#javaScript").hide();
     $("#or").hide();
     $("#cSharp").hide();
     $("#python").hide();
     $("#cPlusPlus").hide();
     $("#allTheAbove").hide();
-    });   
   });
 });
